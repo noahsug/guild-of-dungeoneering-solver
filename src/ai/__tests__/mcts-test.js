1ee52a2232e7083641c5a3a8c7717285ea3fc7ab
sugarman <sugarman@google.com>
2015-12-18 15:54:27 -0500

    removed Monte Carlo Tree Search

--- src/ai/__tests__/mcts-test.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('../mcts');
-
-xdescribe('mcts', () => {
-  const Mcts = require('../mcts');
-  it('should do stuff', () => {
-    const mcts = new Mcts();
-    expect(mcts).toBeDefined();
-  });
-});
